817f9feb07e0230d0cc6a4e88d3bc081db66a5be
Andrew Pegg <andrewcomputsci2019@gmail.com>
2024-04-24 20:47:27 -0700

    added more detailed comments

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//const selectors, getting elements from DOM
 /** @type {HTMLElement} */
 const daysGrid = document.querySelector(".day_grid"),
 currentDate = document.querySelector(".title_bar span"),
@@ -9,49 +10,58 @@ currentDay = date.getDate(),
 currYear = date.getFullYear(),
 currMonth = date.getMonth();
 
-// storing full name of all months in array
+// storing String names of all months in array
 const months = ["January", "February", "March", "April", "May", "June", "July",
               "August", "September", "October", "November", "December"];
-              
+/**
+ * Renders/Creates the content of the Calendar
+ */            
 const renderCalendar = () => {
     let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month, in terms of week days
     // zero indexed, ie 0 = sun, 1 = mon ... 6 = sat
     lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month, ie 31
     lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month, ie what week day
     lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
-    let spanList = [];
+    let spanList = []; //houses all newly created HTML elements
 
-    for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
+    // creating span elements of previous month prefix days of this month
+    for (let i = firstDayofMonth; i > 0; i--) { 
         /** @type {HTMLSpanElement} */
         let dayEle = document.createElement('span');
         dayEle.className = "inactive";
         dayEle.innerText = (lastDateofLastMonth - i + 1);
         spanList.push(dayEle);
     }
+
+
     let isCorrectMonth = currMonth === new Date().getMonth();
     let isCorrectYear = currYear === new Date().getFullYear();
-    
-    for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
-        // adding active class to li if the current day, month, and year matched
+    // create all days of the current month in span elements
+    for (let i = 1; i <= lastDateofMonth; i++) { 
         let isToday = i === currentDay && isCorrectMonth
                      && isCorrectYear ? "active" : "";
+        // if the users current date matches above date then give it the active class
         let dayEle = document.createElement('span');
         dayEle.className = isToday;
         dayEle.innerText = (i);
         spanList.push(dayEle);
     }
-
-    for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
+     // creating spans of next months suffix days of this current month
+    for (let i = lastDayofMonth; i < 6; i++) {
         let dayEle = document.createElement('span');
         dayEle.className = "inactive";
         dayEle.innerText = (i - lastDayofMonth + 1);
         spanList.push(dayEle);
     }
-    currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
-    if(daysGrid.classList.contains("loadingIn")){ //animation toggle
+    // Replace Old date text with the newly updated date
+    currentDate.innerText = `${months[currMonth]} ${currYear}`; 
+    // animation toggle, making sure if animation loading was playing before it is removed before we unload the calendar
+    if(daysGrid.classList.contains("loadingIn")){ 
         daysGrid.classList.remove("loadingIn");
     }
-    daysGrid.classList.add("loading"); // start the loading animation 
+    // start the loading animation 
+    daysGrid.classList.add("loading"); 
+    //update event listener to render the span list to the calendar grid, once the load out animation ended
     daysGrid.onanimationend = () => {
         if(daysGrid.classList.contains("loading")){
             daysGrid.replaceChildren();
@@ -61,21 +71,23 @@ const renderCalendar = () => {
         }
     }
 }
+// implicitly call the render calendar function once the script been loaded by the page
 renderCalendar();
 
+/** added on click listener to update the calendar content, forward or backwards */
 prevNextIcon.forEach(icon => { // getting prev and next icons
     icon.addEventListener("click", () => { // adding click event on both icons
         // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
         currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
 
-        if(currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
-            // creating a new date of current year & month and pass it as date value
+        if(currMonth < 0 || currMonth > 11) { // roll over, and update year and month information
             date = new Date(currYear, currMonth, new Date().getDate());
             currYear = date.getFullYear(); // updating current year with new date year
             currMonth = date.getMonth(); // updating current month with new date month
         } else {
-            date = new Date(); // pass the current date as date value
+            // seems redundant, should be able to just removed this
+            date = new Date(); 
         }
-        renderCalendar(); // calling renderCalendar function
+        renderCalendar(); // calling renderCalendar function, with updated dates
     });
 });
